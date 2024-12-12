ffe62d9ff8957ed5c187f3bfd28efd56e2f3d239
Amr.Hamdallah <amr.hamdallah89@gmail.com>
2024-12-12 02:09:04 +0200

    no commit

---
+++ rectangel.java
@@ -0,0 +1,20 @@
+public class rectang {
+    private double length;
+    private double width;
+    public void setLength(doublel)
+    length=l;
+    public void setWidth(double w) {
+        width = w;
+    }
+    public double getLength() {
+        return length;
+    }
+
+    public double getWideth() {
+        return wideth;
+    }
+    public double getArea() {
+        return area = length * wideth;
+    }
+
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+// we create more than one object from class rectangle which its variables(width length)
+            Rectangle r1 = new Rectangle();
+            r1.width = (6);
+            r1.height = (5);
+            r1.getHeight();
+            r1.getWidth();
+            r1.getSize();
+
+            Rectangle r2 = new Rectangle();
+            r2.width = (8);
+            r2.height = (9);
+            r1.getHeight();
+            r1.getWidth();
+            r1.getSize();
+
+            Rectangle r3 = new Rectangle();
+            r3.width = (4);
+            r3.height = (2);
+            r1.getHeight();
+            r1.getWidth();
+            r1.getSize();
+
+        }
+    }
